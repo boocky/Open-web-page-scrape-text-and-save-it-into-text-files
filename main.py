7ccdf16c1a53aef3f233a04f00a31973cab98b32
Slavoljub “Buco” <boocky@gmail.com>
2022-12-05 17:12:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# Description
+# The script will Login form and click Home
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def get_driver():
+  # Set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",
+  ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+  
+  driver = webdriver.Chrome(options=options)
+  driver.get("https://automated.pythonanywhere.com/login/")
+  return driver
+
+def main():
+  driver = get_driver()
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  # print(driver.current_url)
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  print(driver.current_url)
+print(main())
